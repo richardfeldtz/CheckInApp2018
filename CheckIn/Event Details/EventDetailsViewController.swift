e4c142d024d9f4c3f39754776f7aa4751c7cea40
Richard Feldtz <richard.c.feldtz@tsgforce.com>
2019-01-24 15:09:31 -0500

    Richard - Updated event details

--- CheckIn/Event Details/EventDetailsViewController.swift
+++ CheckIn/Event Details/EventDetailsViewController.swift
@@ -12,11 +12,29 @@ class EventDetailsViewController: UIViewController {
     
     @IBOutlet weak var checkInCount: UILabel!
     @IBOutlet weak var guestCount: UILabel!
+	
     override func viewDidLoad() {
-        super.viewDidLoad()
+		super.viewDidLoad();
+		let coreData = CoreDataHelper.retrieveData("Checkins");
+		updateCheckInCount(checkInData: coreData as! [Checkins]);
+		updateGuestCount(checkInData: coreData as! [Checkins]);
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+	
+	func updateCheckInCount(checkInData: [Checkins]) {
+		//print(checkInData);
+		checkInCount.text = String(checkInData.count);
+		//TODO figure out why it checks in 2 students when you check in a student with guests
+	}
+	
+	func updateGuestCount(checkInData: [Checkins]) {
+		var totalGuests = Int16();
+		for checkIns in checkInData {
+			totalGuests += checkIns.guests;
+		}
+		guestCount.text = String(totalGuests);
+	}
 }
