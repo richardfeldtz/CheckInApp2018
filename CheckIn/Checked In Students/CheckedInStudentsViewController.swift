5c23e10cc49736368077a379074bc62eddef58bf
Richard Feldtz <richard.c.feldtz@tsgforce.com>
2019-01-29 15:20:54 -0500

    Updated flow for checked in students page

---
+++ CheckIn/Checked In Students/CheckedInStudentsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CheckedInStudentsViewController.swift
+//  CheckIn
+//
+//  Created by Richard Feldtz on 1/28/19.
+//  Copyright © 2019 Anand Kelkar. All rights reserved.
+//
+
+import UIKit
+
+class CheckedInStudentsViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad();
+	}
+	
+}
